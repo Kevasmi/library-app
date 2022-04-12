c2b4c50ff31b796ace63f2b1b9e58b25e88371ec
Richard Lasco <lasco9@gmail.com>
2022-04-12 13:59:19 -0500

    Added new book functionality

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const main = document.querySelector('main')
 const newBook = document.querySelector('.new-book');
 const modalContainer = document.querySelector('.modal-container');
 const closeBtn = document.querySelector('.close-btn');
+const addBook = document.querySelector('#submit-btn');
 
 newBook.addEventListener('click', () => {
     modalContainer.classList.add('show');
@@ -25,7 +27,48 @@ function Book(title, author, pages) {
 function addBookToLibrary(title, author, pages) {
     let newBook = new Book(title, author, pages)
     myLibrary.push(newBook);
+    console.log(myLibrary)
+} 
+
+function closeModal() {
+    modalContainer.classList.remove('show');
 }
+
+function clearForm() {
+    title.value = ''
+    author.value = ''
+    pages.value = ''
+}
+
+function addCard(title, author, pages) {
+    const card = document.createElement('div')
+    const cardTitle = document.createElement('h2')
+    const cardAuthor = document.createElement('h3')
+    const cardPages = document.createElement('p')
+    const cardRead = document.createElement('p')
+    cardTitle.textContent = title
+    cardAuthor.textContent = author
+    cardPages.textContent = `${pages} pages`
+    cardRead.textContent = 'read'
+    card.classList.add('card')
+    main.appendChild(card)
+    card.appendChild(cardTitle)
+    card.appendChild(cardAuthor)
+    card.appendChild(cardPages)
+    card.appendChild(cardRead)
+}
+
+addBook.addEventListener('click', (e) => {
+    e.preventDefault();
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    addBookToLibrary(title, author, pages);
+    closeModal();
+    clearForm();
+    addCard(title, author, pages);
+})
+
 
 // addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
 
